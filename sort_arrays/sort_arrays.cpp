2973f20bd035b4cc98c008c2b38928170f6c4e8a
Mutero Kiberi <trinexusco@gmail.com>
2021-06-11 11:58:20 +0300

    program to sort arrays

--- sort_arrays/sort_arrays.cpp
+++ sort_arrays/sort_arrays.cpp
@@ -1,20 +1,38 @@
-// sort_arrays.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// largest.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int myarr[5] = { 1, 18, 12, 97, 45 };
+	int arrSize = sizeof(myarr[0]);
+
+
+	//sorting
+	//get var a and b(
+	int smaller = 0;
+	int larger = 0;
+
+	for (int i = 0; i <= arrSize; i++)
+	{
+		smaller = myarr[i - 1];
+		if (myarr[i] < smaller)
+		{
+			larger = smaller;
+			smaller = myarr[i];
+
+			myarr[i] = larger;
+			myarr[i - 1] = smaller;
+
+		}
+	}
+
+	for (int i = 0; i <= arrSize; i++)
+	{
+		std::cout << myarr[i] << " ";
+	}
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
